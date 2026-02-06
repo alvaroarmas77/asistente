371891f20f5d4b0b2625eb3f34a5f9595ba4da84
unknown <alvaroarmas@gmail.com>
2026-02-06 17:20:14 -0500

    viernes6-1720

--- src/asistente_agenda/crew.py
+++ src/asistente_agenda/crew.py
@@ -44,6 +44,7 @@ class AsistenteAgendaCrew:
             model="gemini/gemini-2.0-flash",
             api_key=os.getenv("GOOGLE_API_KEY"),
             temperature=0.5
+            max_rpm=10
         )
 
     @agent
